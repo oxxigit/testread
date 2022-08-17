9bafac5e6e88e92ccb268b646c60e40f3cdda145
Stefano Manto <95471672+oxxigit@users.noreply.github.com>
2022-08-17 20:06:54 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Progetto di prova OXXI'
+copyright = '2022, OXXI'
+author = 'Stefano Manto'
 
 release = '0.1'
 version = '0.1.0'
